1f31fd8fb7b4f958ba55c5b1e3615d97c67abb20
Jonathan House <jonlhouse@gmail.com>
2012-02-06 17:36:36 -0600

    local changes

---
+++ install.sh
@@ -0,0 +1,15 @@
+#
+# Copy generated bootstrap files to ror/strongmd locations
+#
+
+BOOTSTRAP_DIR="/home/jonlhouse/dev/bootstrap"
+STRONGMD_DIR="/home/jonlhouse/ror/strongmd"
+
+cd $BOOTSTRAP_DIR
+make
+echo "Copying files to $STRONGMD_DIR"
+cp docs/assets/css/bootstrap* "$STRONGMD_DIR/vendor/assets/stylesheets/bootstrap"
+cp js/bootstrap* "$STRONGMD_DIR/vendor/assets/javascripts/bootstrap"
+cp img/glyph* "$STRONGMD_DIR/public/img"
+echo "Finished!"
+
