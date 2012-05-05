5011828b484a65744e6a632a2728901dbe2f5e64
John House <jonlhouse@gmail.com>
2012-05-04 21:30:42 -0500

    some changes

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ make
 echo "Copying files to $STRONGMD_DIR"
 cp docs/assets/css/bootstrap* "$STRONGMD_DIR/vendor/assets/stylesheets/bootstrap"
 cp js/bootstrap* "$STRONGMD_DIR/vendor/assets/javascripts/bootstrap"
-cp img/glyph* "$STRONGMD_DIR/public/img"
+cp img/glyph* "$STRONGMD_DIR/vendor/assets/stylesheets/bootstrap"
+cp img/glyph* "$STRONGMD_DIR/vendor/assets/"
 echo "Finished!"
