a8d09e3893c075171c6fb2701c7b158227868621
John House <jonlhouse@gmail.com>
2012-05-15 00:26:26 -0500

    responsive changes for StrongMD

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ BOOTSTRAP_DIR="/home/jonlhouse/rails/bootstrap"
 STRONGMD_DIR="/home/jonlhouse/rails/strongmd"
 
 cd $BOOTSTRAP_DIR
-make
+make bootstrap
 echo "Copying files to $STRONGMD_DIR"
-cp docs/assets/css/bootstrap* "$STRONGMD_DIR/vendor/assets/stylesheets/bootstrap"
-cp js/bootstrap* "$STRONGMD_DIR/vendor/assets/javascripts/bootstrap"
-cp img/glyph* "$STRONGMD_DIR/vendor/assets/stylesheets/bootstrap"
+cp bootstrap/css/* "$STRONGMD_DIR/vendor/assets/stylesheets"
+cp bootstrap/js/* "$STRONGMD_DIR/vendor/assets/javascripts"
+cp img/glyph* "$STRONGMD_DIR/vendor/assets/stylesheets"
 cp img/glyph* "$STRONGMD_DIR/vendor/assets/"
 echo "Finished!"
